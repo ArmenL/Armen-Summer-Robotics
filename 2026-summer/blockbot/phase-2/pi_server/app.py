08878a0cf565fe0a7e674e09079f4fe64a7500e7
ArmenL <armen.avagyan.first@gmail.com>
2026-06-24 11:18:04 -0700

    index.html created with connection indicator and live values from app.py. Also updated app.py to be compatible with index.html.

--- 2026-summer/blockbot/phase-2/pi_server/app.py
+++ 2026-summer/blockbot/phase-2/pi_server/app.py
@@ -1,13 +1,16 @@
 import fastapi
+from fastapi.responses import HTMLResponse
 app = fastapi.FastAPI(title="Pi Serial API")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def get_home():
-    return {"message": "Hello, World!"}
+    with open("pi_server/index.html") as f:
+        content = f.read()
+    return content
 
 @app.get("/telemetry")
 def get_telemetry():
-    return {"type":"telemetry","battery_v":7.42,"left_ticks":1234,"right_ticks":1211,"tof_mm":642,"estop":False}
+    return {"type":"telemetry","battery_v":7.42,"left_ticks":1234,"right_ticks":1211,"tof_mm":642,"estop":"off"}
 
 @app.get("/extra")
 def get_extra():
